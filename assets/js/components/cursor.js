9cba683bbbed16689c32896184f7ee6424d2f38c
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2022-09-27 10:52:52 +0700

    fix some bug

--- assets/js/components/cursor.js
+++ assets/js/components/cursor.js
@@ -19,9 +19,17 @@ let labels = Array.from(document.querySelectorAll("label"));
 
 links.forEach((link) => {
   link.addEventListener("mouseover", () => {
+    document.querySelector(".inner-cursor p").style.opacity = `1`;
+    document.querySelector(
+      ".inner-cursor p"
+    ).style.transform = `rotate(-45deg) scale(1)`;
     innerCursor.classList.add("grow");
   });
   link.addEventListener("mouseleave", () => {
+    document.querySelector(".inner-cursor p").style.opacity = `0`;
+    document.querySelector(
+      ".inner-cursor p"
+    ).style.transform = `rotate(-45deg) scale(0)`;
     innerCursor.classList.remove("grow");
   });
 });
